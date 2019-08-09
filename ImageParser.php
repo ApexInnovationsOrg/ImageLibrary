9c9c2c4688e71b5df3da338e5af201453840b329
JohnKlein985 <John@apexinnovations.com>
2019-08-09 11:32:10 -0500

    Thumbnail changes, save in advance

--- ImageParser.php
+++ ImageParser.php
@@ -17,16 +17,25 @@ class ImageParser
 	
 	public function parse()
 	{
-		
-		$newThumb = new Imagick($this->src);		
-		$imgWidth = $newThumb->getImageWidth();
-		$imgHeight = $newThumb->getImageHeight();				
-		$newThumb->thumbnailImage(100,100,true);
-		
-		$mimeType = $newThumb->getImageMimeType();
-		
-		header('Content-type: ' . $mimeType);	
-		echo $newThumb;
+		//Check if image thumbnail exists. If not, create it, save it, then pull it.
+		$src = explode("SortedAssets/",$this->src);
+		$newSrc = "Thumbnails/" . $src[1];
+		if(!file_exists($newSrc)){
+			$newThumb = new Imagick($this->src);		
+			$imgWidth = $newThumb->getImageWidth();
+			$imgHeight = $newThumb->getImageHeight();				
+			$newThumb->thumbnailImage(100,100,true);	
+			
+			$directoryStructure = dirname($newSrc);			
+			$fileName = basename($newSrc);
+			if(!file_exists($directoryStructure)){
+				mkdir($directoryStructure,0755,true);	
+			}
+			$newThumb->writeImage($newSrc);
+		}
+				
+		header('Content-type: ' . mime_content_type($newSrc));	
+		readfile($newSrc);
 		
 	}	
 	
