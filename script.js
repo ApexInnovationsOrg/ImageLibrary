32d107507b1a39c04b7b37c249f77fbd31ad7b5e
JohnKlein985 <John@apexinnovations.com>
2019-08-09 15:26:30 -0500

    Popout url of full size image

--- script.js
+++ script.js
@@ -19,7 +19,10 @@ $(function() {
 			
 			//Clicked an image panel
 			$('.imgPanel').click(function(){
+				var folder = $(this).attr("data-folder");
+				var title = $(this).attr("title");				
 				
+				PopupCenter('SortedAssets/' + folder + '/' + title,'xtf','900','500');  
 			});				
 			
 			//Once all files processed, lets make our selectable dropdown
@@ -131,6 +134,25 @@ $(function() {
 			
 	});
 });
+
+function PopupCenter(url, title, w, h) {
+	// Fixes dual-screen position                         Most browsers      Firefox
+	var dualScreenLeft = window.screenLeft != undefined ? window.screenLeft : window.screenX;
+	var dualScreenTop = window.screenTop != undefined ? window.screenTop : window.screenY;
+
+	var width = window.innerWidth ? window.innerWidth : document.documentElement.clientWidth ? document.documentElement.clientWidth : screen.width;
+	var height = window.innerHeight ? window.innerHeight : document.documentElement.clientHeight ? document.documentElement.clientHeight : screen.height;
+
+	var systemZoom = width / window.screen.availWidth;
+	var left = (width - w) / 2 / systemZoom + dualScreenLeft
+	var top = (height - h) / 2 / systemZoom + dualScreenTop
+	var newWindow = window.open(url, title, 'scrollbars=yes, width=' + w / systemZoom + ', height=' + h / systemZoom + ', top=' + top + ', left=' + left);
+
+	// Puts focus on the newWindow
+	if (window.focus) newWindow.focus();
+}
+
+
 
 //Create image type selector section
 function createImageTypes(){
